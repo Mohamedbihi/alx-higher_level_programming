7e9be55aa9ef28f3a23d3bf3c19a551a6a795511
Mohamedbihi <simobihi72@gmail.com>
2023-08-08 07:10:54 -0700

    9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 9-print_last_digit.py
+
+def print_last_digit(number):
+    """Print the last digit of a number and return it."""
+    # Calculate the absolute value of the number and find the
+# remainder when divided by 10
+    last_digit = abs(number) % 10
+    # Print the last digit without a newline character
+    print(last_digit, end="")
+    # Return the last digit
+    return last_digit
