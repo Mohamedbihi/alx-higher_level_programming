3856b5dcbb65296d264129cce347a2b26cfddbaf
mohamedbihi <simobihi72@gmail.com>
2023-11-14 06:09:33 -0800

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,10 @@
-6-multi_languages_loop.js
+#!/usr/bin/node
+
+// script to print strings using a loop
+
+let i;
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (i = 0; i < 3; i++) {
+  console.log(myString[i]);
+}
